2d67ad41ef0cf2cdf1f26f84fdc9963082208e7d
Isra-git <villar_80@hotmail.com>
2025-11-25 20:30:53 +0100

    feature/richTextEditorStart

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -3,6 +3,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import RichTextEditor from '../forms/rich-text-editor';
+
 export default class BlogForm extends Component {
   constructor(props) {
     super(props);
@@ -10,10 +12,17 @@ export default class BlogForm extends Component {
     this.state = {
       title: '',
       blog_status: '',
+      content: '',
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this);
+  }
+
+  // maneja los cambios en el editor de texto DraftJS
+  handleRichTextEditorChange(content) {
+    this.setState({ content }); // content:content
   }
 
   // constructor del formularios
@@ -21,6 +30,7 @@ export default class BlogForm extends Component {
     let formData = new FormData();
     formData.append('portfolio_blog[title]', this.state.title);
     formData.append('portfolio_blog[blog_status]', this.state.blog_status);
+    formData.append('portfolio_blog[content]', this.state.content);
 
     return formData;
   }
@@ -71,7 +81,9 @@ export default class BlogForm extends Component {
             value={this.state.blog_status}
           />
         </div>
-
+        <div className="one-column">
+          <RichTextEditor handleRichTextEditorChange={this.handleRichTextEditorChange} />
+        </div>
         <button className="btn">Save</button>
       </form>
     );
---
+++ src/components/forms/rich-text-editor.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+// importaciones de la biblioteca DraftJS, para editor de texto
+import { EditorState, convertToRaw } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
+
+export default class RichTextEditor extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
+  }
+
+  // se pasa al editor como props para manejar, el segundo argumento es una func. que
+  // espera que se actualice el estado antes de llamarse (setState->Asincrono)
+  onEditorStateChange(editorState) {
+    this.setState(
+      { editorState },
+      this.props.handleRichTextEditorChange(
+        draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
+      )
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Editor
+          editorState={this.state.editorState}
+          wrapperClassName="demo-wrapper"
+          editorClassName="demo-editor"
+          onEditorStateChange={this.onEditorStateChange}
+        />
+      </div>
+    );
+  }
+}

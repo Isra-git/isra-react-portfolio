b629eb4b95bb4d622ae442a80b8da0c5a4ce79e0
Isra-git <villar_80@hotmail.com>
2025-12-15 19:02:01 +0100

    feature/authStylesMedia

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        email: "",
-        password: "",
-        errorText: ""
+      email: '',
+      password: '',
+      errorText: '',
     };
 
     //Bindear las funciones para que tengan acceso al this
@@ -17,73 +19,82 @@ export default class Login extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-   handleChange(event) {
-  this.setState({
-    [event.target.name]: event.target.value,
-    errorText:"" // Limpiamos el mensaje de error cuando el usuario escribe
-  });
-}
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+      errorText: '', // Limpiamos el mensaje de error cuando el usuario escribe
+    });
+  }
 
-
-    handleSubmit(event) {
-        
-        axios.post("https://api.devcamp.space/sessions", {
-            client: {
-                email: this.state.email,
-                password: this.state.password
-            }
-        }, { withCredentials: true }
-            )
-            .then(response => {  // con withCredentials decimos que queremos enviar las cookies al servidor
-                if (response.data.status === 'created') {
-                  this.props.handleSuccessfulAuth(this.state.email); // Llamamos a la funcion que maneja el login exitoso
-
-                } else {
-                  this.setState({ errorText: "Wrong email or password" });
-                  this.props.handleUnSuccessfulAuth(); // Llamamos a la funcion que maneja el login no exitoso
-                }
-            })
-            .catch(error => {   // capturamos cualquier error que pueda ocurrir
-                 
-                this.setState({                                     
-                    errorText: "An error occurred. Please try again."
-                });
-                this.props.handleUnSuccessfulAuth(); // Llamamos a la funcion que maneja el login no exitoso
-            });
-
-        event.preventDefault(); // Evita que el formulario se envie y recargue la pagina
+  handleSubmit(event) {
+    axios
+      .post(
+        'https://api.devcamp.space/sessions',
+        {
+          client: {
+            email: this.state.email,
+            password: this.state.password,
+          },
+        },
+        { withCredentials: true }
+      )
+      .then(response => {
+        // con withCredentials decimos que queremos enviar las cookies al servidor
+        if (response.data.status === 'created') {
+          this.props.handleSuccessfulAuth(this.state.email); // Llamamos a la funcion que maneja el login exitoso
+        } else {
+          this.setState({ errorText: 'Wrong email or password' });
+          this.props.handleUnSuccessfulAuth(); // Llamamos a la funcion que maneja el login no exitoso
         }
+      })
+      .catch(error => {
+        // capturamos cualquier error que pueda ocurrir
 
-        
+        this.setState({
+          errorText: 'An error occurred. Please try again.',
+        });
+        this.props.handleUnSuccessfulAuth(); // Llamamos a la funcion que maneja el login no exitoso
+      });
+
+    event.preventDefault(); // Evita que el formulario se envie y recargue la pagina
+  }
+
   render() {
-
     return (
-      <div>
-         <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
-         <div className='warning-text'>{this.state.errorText}</div>
+      <div className="right-column">
+        <div className="login-title">
+          <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+        </div>
+        <div className="warning-text">{this.state.errorText}</div>
 
-        <form onSubmit={this.handleSubmit}  className='portfolio-form-wrapper'>
-            <input 
-                className=''
-                type="text"
-                name="email" // Tiene que coincidir con el nombre del estado {email}
-                placeholder="Your email"
-                value={this.state.email} 
-                onChange={this.handleChange} // Actualiza el estado cuando el usuario escribe
+        <form onSubmit={this.handleSubmit} className="auth-form-wrapper">
+          <div className="form-group">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <input
+              className=""
+              type="text"
+              name="email" // Tiene que coincidir con el nombre del estado {email}
+              placeholder="Your email"
+              value={this.state.email}
+              onChange={this.handleChange} // Actualiza el estado cuando el usuario escribe
             />
-              <input 
-                type="password"
-                name="password" // Tiene que coincidir con el nombre del estado {password}
-                placeholder="Your password"
-                value={this.state.password} 
-                onChange={this.handleChange} // Actualiza el estado cuando el usuario escribe
-            /> 
-
-            <div className='btn-auhth-wrapper'>
-                 <button type="submit" className='btn'>Login</button>
-        
-            </div>
-        </form> 
+          </div>
+          <div className="form-group">
+            <FontAwesomeIcon icon={faLock} />
+            <input
+              type="password"
+              name="password" // Tiene que coincidir con el nombre del estado {password}
+              placeholder="Your password"
+              value={this.state.password}
+              onChange={this.handleChange} // Actualiza el estado cuando el usuario escribe
+            />
+          </div>
+          <div className="btn-auhth-wrapper">
+            <button type="submit" className="btn">
+              Login
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,56 +1,47 @@
-import React, {Component} from "react";
-import Login from "../auth/login";
+import React, { Component } from 'react';
+import Login from '../auth/login';
 
 // importamos la imagen de fondo del login
-import loginImg from "../../../static/assets/images/auth/login.jpg";
+import loginImg from '../../../static/assets/images/auth/login.png';
 
 export default class Auth extends Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.handleSuccessfulAuth=this.handleSuccessfulAuth.bind(this);
-        this.handleUnSuccessfulAuth=this.handleUnSuccessfulAuth.bind(this);
+    this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+    this.handleUnSuccessfulAuth = this.handleUnSuccessfulAuth.bind(this);
+  }
 
-    }
+  //Si el login es exitoso, llamamos a la funcion que nos pasan por props desde app.js
+  // y redirigimos al usuario a la pagina principal
+  handleSuccessfulAuth(email) {
+    this.props.handleSuccessfulLogin(email);
+    this.props.history.push('/');
+  }
 
-    //Si el login es exitoso, llamamos a la funcion que nos pasan por props desde app.js
-    // y redirigimos al usuario a la pagina principal
-    handleSuccessfulAuth(email){
-        this.props.handleSuccessfulLogin(email);
-        this.props.history.push("/");
+  //Si el login no es exitoso, llamamos a la funcion que nos pasan por props desde app.js
+  handleUnSuccessfulAuth() {
+    this.props.handleUnSuccessfulLogin();
+  }
 
-    }
+  render() {
+    return (
+      <div className="auth-page-wrapper">
+        <div
+          className="left-column"
+          style={{
+            backgroundImage: `url(${loginImg})`,
+          }}
+        />
 
-    //Si el login no es exitoso, llamamos a la funcion que nos pasan por props desde app.js
-    handleUnSuccessfulAuth(){
-        this.props.handleUnSuccessfulLogin();
-    }
-
-    render(){
-
-        return(
-            <div className="auth-page-wrapper">
-                <div className="left-column"
-                style={{
-                    backgroundImage: `url(${loginImg})`,
-                    
-                  }}
-                
-                />
-
-                
-
-                <div className="right-column">
-
-                    {/* Pasamos al componente login los props que necesita para manejar la autenticacion */}
-                    <Login
-                    handleSuccessfulAuth={this.handleSuccessfulAuth}
-                    handleUnSuccessfulAuth={this.handleUnSuccessfulAuth}
-
-                    />
-                </div>
-
-            </div>
-        );
-    }
+        <div className="right-column">
+          {/* Pasamos al componente login los props que necesita para manejar la autenticacion */}
+          <Login
+            handleSuccessfulAuth={this.handleSuccessfulAuth}
+            handleUnSuccessfulAuth={this.handleUnSuccessfulAuth}
+          />
+        </div>
+      </div>
+    );
+  }
 }
